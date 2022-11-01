6d1a991c361d1c770cea02834540b579403dfa21
Héctor Vinasco <hectorvinasco12@gmail.com>
2022-10-31 20:43:56 -0500

    Cuarta prueba retorno home

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/spa-RM">
+          <a href="#">
             SPA
           </a>
         </h1>
